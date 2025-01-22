6da64ddebff8a298ed12629482bb1edf6a4b99c8
Halil Karatas <ahmetkaratasdev@gmail.com>
2025-01-22 20:50:33 +1100

    Adding scrapeLogic

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -2,55 +2,56 @@ const puppeteer = require("puppeteer");
 require("dotenv").config();
 
 const scrapeLogic = async (res) => {
+  const browser = await puppeteer.launch({
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
+  try {
+    const page = await browser.newPage();
 
-    const browser = await puppeteer.launch({
-        args: [
-            "--disable-setuid-sandbox",
-            "--no-sandbox",
-            "--single-process",
-            "--no-zygote"
-        ],
-        // If we are in production, then set puppeteer path in environment variables. Otherwise, use default executable path. 
-        executablePath: process.env.NODE_ENV === 'production' ? process.env.PUPPETEER_EXECUTABLE_PATH : puppeteer.executablePath(),
-    });
-    try {   
-        const page = await browser.newPage();
+    await page.goto('https://developer.chrome.com/');
 
-        await page.goto('https://developers.google.com/web/');
+    // Set screen size
+    await page.setViewport({width: 1080, height: 1024});
 
-        // Type into search box.
-        await page.type('.devsite-search-field', 'Headless Chrome');
+    // Type into search box
+    await page.type('.devsite-search-field', 'automate beyond recorder');
 
-        // Wait for suggest overlay to appear and click "show all results".
-        const allResultsSelector = '.devsite-suggest-all-results';
-        await page.waitForSelector(allResultsSelector);
-        await page.click(allResultsSelector);
+    // Wait and click on first result
+    const searchResultSelector = '.devsite-result-item-link';
+    await page.waitForSelector(searchResultSelector);
+    await page.click(searchResultSelector);
 
-        // Wait for the results page to load and display the results.
-        const resultsSelector = '.gsc-table-result a.gs-title[href]';
-        await page.waitForSelector(resultsSelector);
+    // Locate the full title with a unique string
+    const textSelector = await page.waitForSelector(
+    'text/Customize and automate'
+    );
+    const fullTitle = await textSelector.evaluate(el => el.textContent);
 
+    // Print the full title
+    console.log('The title of this blog post is "%s".', fullTitle);
+    res.status(200).send(`The title of this blog post is ${fullTitle}`);
+    await browser.close();
 
-        // Extract the results from the page.
-        const links = await page.evaluate(resultsSelector => {
-            const anchors = Array.from(document.querySelectorAll(resultsSelector));
-            return anchors.map(anchor => {
-            const title = anchor.textContent.split('|')[0].trim();
-            return `${title} - ${anchor.href}`;
-            });
-        }, resultsSelector);
-        console.log(links.join('\n'));
-        res.send("Working all fine");
-    } catch (e) {
-        console.error(e);
-        res.send(`Something went wrong while running puppeteer ${e}`);
-    } finally {   
-        await browser.close();
-    }
+    
+  } catch (e) {
+    console.error(e);
+    res.send(`Something went wrong while running Puppeteer: ${e}`);
+  } finally {
+    await browser.close();
+  }
+};
+
+const delay = async (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-    const delay = async (ms) => {
-        return new Promise(resolve => setTimeout(resolve, ms));
-    };
-
-module.exports = {scrapeLogic};
+module.exports = { scrapeLogic };
